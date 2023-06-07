154baca074749e78ee9d34a5c73bc7bb3bb1ba45
Nafie Asfour <ph.nafee.asfour@gmail.com>
2023-06-07 19:53:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+let messages = [];
+
+function writeCards(names, event){
+    for (let i = 0; i<names.length; i++){
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        messages.push(message);
+    }
+    return messages;
+}
+function countDown(number){
+    for (let i = number; i >= 0; i-- ){
+        console.log(i)
+    }
+}
